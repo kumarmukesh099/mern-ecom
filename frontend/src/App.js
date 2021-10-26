edd4572a372c853c8c5f0e8b61292d60131ad956
mukesh kumar <mk380345@gmail.com>
2021-10-26 22:25:30 +0530

    react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div style={{textAlign:"center", margin:'0:auto'}}>
+      My Ecommerce Started
+    </div>
+  )
+}
+
+export default App
